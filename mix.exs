4f580fad95f28ae5f3719ca626782cf4de687577
scorsi <sylvain.corsini@protonmail.com>
2020-05-31 22:23:12 +0200

    Update mix.exs to add ex_doc and sufficiant attributes for hexpm

--- mix.exs
+++ mix.exs
@@ -4,15 +4,19 @@ defmodule Umbra.MixProject do
   def project do
     [
       app: :umbra,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      name: "Umbra",
+      version: "0.0.1",
+      elixir: "~> 1.7",
+      source_url: "https://github.com/scorsi/Umbra",
+      description: description(),
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib","test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   def application do
@@ -21,8 +25,24 @@ defmodule Umbra.MixProject do
     ]
   end
 
+  defp description do
+    "Umbra helps you make your GenServer rocks in lesser code"
+  end
+
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      name: "umbra",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/elixir-ecto/postgrex"
+      }
     ]
   end
 end
